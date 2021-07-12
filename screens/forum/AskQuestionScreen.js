83a18f7a6b49877b3ce879594b14f465ee3e55dd
AsadGohar <53337734+AsadGohar@users.noreply.github.com>
2021-07-12 23:34:18 +0500

    updates to forum
    added Ask A Question form, updated topic question screen, and updated Topic Section in forum screen

--- screens/forum/AskQuestionScreen.js
+++ screens/forum/AskQuestionScreen.js
@@ -1,12 +1,170 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React,{useEffect,useState} from 'react'
+import { Text, View ,StyleSheet,TextInput,ToastAndroid,LogBox} from 'react-native'
+import DropDownPicker from 'react-native-dropdown-picker';
+import axios from "../../components/supportComponents/axios"
+import { Button, Input } from 'react-native-elements'
+import { useSelector } from 'react-redux'
 
-const AskQuestionScreen = () => {
+
+
+const AskQuestionScreen = ({navigation}) => {
+
+    const userInfo = useSelector(state => state.user.userInfo)
+
+    const [submitLoader, setSubmitLoader] = useState(false)
+    const [topicPickerOpen, settopicPickerOpen] = useState(false)
+
+    const [topic, setTopicValue] = useState('')
+    const [statement, setStatement] = useState('')
+    const [description, setDescription] = useState('')
+
+    const [topics, setTopics] = useState([
+        {label:"Transport",value:"Transport"},
+        {label:"Accomodations",value:"Accomodations"},
+        {label:"Budget",value:"Budget"},
+        {label:"Other",value:"Other"}
+    ])
+
+    useEffect(()=>{
+        LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
+
+    },[])
+
+    const addQuestion = () => {
+
+        setSubmitLoader(true)
+        if ( topic=='' || statement=='' || description=='' ){
+            setSubmitLoader(false)
+            ToastAndroid.show(
+                'Please Complete the Form',
+                ToastAndroid.LONG,
+                ToastAndroid.BOTTOM
+            );
+        }
+        else if (userInfo) {
+          const user = userInfo._id
+          axios.post('/questions/', { topic, statement, description, user })
+            .then(res => {
+                setSubmitLoader(false)
+                ToastAndroid.show(
+                    'Question Added',
+                    ToastAndroid.LONG,
+                    ToastAndroid.BOTTOM
+                );
+                navigation.navigate('QuestionDetails', {
+                    questionID: res.data._id
+                })
+            })
+            .catch(err => {
+                setSubmitLoader(false)
+            })
+        }
+        else {
+            setSubmitLoader(false)
+            ToastAndroid.show(
+                'Please Log In to Submit Question',
+                ToastAndroid.LONG,
+                ToastAndroid.BOTTOM
+            );
+        }
+    
+      }
     return (
-        <View>
-            <Text>Ask Question</Text>
+        <View style={styles.container}>
+            <Text style={styles.label}>Topic</Text>
+            <DropDownPicker
+                open={topicPickerOpen}
+                value={topic}
+                items={topics}
+                setOpen={settopicPickerOpen}
+                setValue={setTopicValue}
+                setItems={setTopics}
+                containerStyle={styles.pickerStyle}
+                zIndex={10001}
+                searchable={false}
+                placeholder='Select Your Topic'
+            />
+            <Text style={styles.label}>Question Statement</Text>
+            <TextInput
+                style={styles.textInput}
+                onChangeText={statement => setStatement(statement)}
+                 multiline={true} numberOfLines={10}
+            />
+            <Text  style={styles.label}>Question Description</Text>
+            <TextInput
+                style={styles.textInput}
+                onChangeText={description => setDescription(description)}
+                 multiline={true} numberOfLines={10}
+            />
+            <Button title='Submit Question' onPress={addQuestion}
+                containerStyle={styles.btnCheckCont} buttonStyle={styles.btnCheck}
+                loading={submitLoader}
+            />
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        alignItems: 'center'
+    },
+    text1: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 6,
+        marginTop: 5
+    },
+    pickerStyle: {
+        width: '90%',
+        marginTop: 10
+    },
+    label: {
+        fontSize: 18,
+        color: '#114B5F',
+        fontWeight: 'bold',
+        marginTop: 10
+    },
+    btnCheckCont: {
+        width: '90%',
+        alignSelf: 'center',
+        marginTop: 20
+    },
+    btnCheck: {
+        backgroundColor: '#1A936F'
+    },
+    input: {
+        paddingTop:20,
+        width: '95%',
+        borderWidth: 0.5,
+        borderColor: "transparent" 
+    },
+    inputStyle: {
+        fontSize: 17,
+        backgroundColor:"white",
+        borderWidth:0
+    },
+    textInput: {
+        height: 80,
+        margin: 12,
+        borderWidth: 0.5,
+        borderColor: 'grey',
+        width: 350,
+        paddingHorizontal: 5,
+        paddingVertical: 5,
+        borderRadius: 5,
+        textAlignVertical: 'top',
+        backgroundColor:"white"
+    },
+    btnCheckCont: {
+        width: '90%',
+        alignSelf: 'center',
+        marginTop: 20
+    },
+    btnCheck: {
+        backgroundColor: '#1A936F'
+    },
+})
 
 export default AskQuestionScreen
--- screens/forum/TopicQuestionsScreen.js
+++ screens/forum/TopicQuestionsScreen.js
@@ -1,12 +1,147 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React,{useEffect,useState} from 'react'
+import { ScrollView, Text, View, TouchableOpacity, StyleSheet, Image, FlatList, ActivityIndicator, LogBox } from 'react-native'
+import {imagePath} from '../../components/supportComponents/axios'
+import axios from "../../components/supportComponents/axios"
 
-const TopicQuestionsScreen = () => {
+const TopicQuestionsScreen = ({route,navigation}) => {
+
+    const [questions, setQuestions] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        axios.get(`/questions/topic/${route.params.topic}`)
+        .then(res => {
+            // console.log(res.data);
+            setQuestions(res.data);
+            setLoading(false)
+        })
+        .catch((err) => {
+            setLoading(false)
+            console.log(err);
+        });
+        LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
+
+    }, [])
+  
     return (
-        <View>
-            <Text>Topics Qs</Text>
-        </View>
+        <ScrollView>
+             <Text style={styles.heading1}>{`${route.params.topic} Questions`}</Text>
+                <View style={styles.allQuestionsConatiner}>
+                    {loading ? (<ActivityIndicator size='large' color='#1A936F' style={{ marginTop: 50 }} />) : (
+                        <FlatList
+                            showsVerticalScrollIndicator={false}
+                            data={questions}
+                            keyExtractor={item => item._id}
+                            initialNumToRender={4}
+                            renderItem={itemData => (
+                                <TouchableOpacity onPress={() => navigation.navigate('QuestionDetails', {
+                                    questionID: itemData.item._id
+                                })}>
+                                    <View style={{ flexDirection: 'column' }}>
+                                        <View style={styles.singleQuestionView}>
+                                            <View style={styles.profileImageView}>
+                                                <Image source={{ uri: `${imagePath}/users/${itemData.item.user.display_image_name}`}} style={styles.profileImage} />
+                                            </View>
+                                            <View style={styles.questionInfoView}>
+                                                <Text style={styles.text2}>{itemData.item.user.name}</Text>
+                                                <Text>{itemData.item.statement}</Text>
+                                                <Text style={styles.dateText}>{`Posted: ${itemData.item.createdAt.substring(0,10)}`}</Text>
+                                            </View>
+                                        </View>
+                                    </View>
+                                </TouchableOpacity>
+                            )}
+                        />
+                    )}
+                </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 50
+    },
+    topicsView: {
+        flexDirection: 'row',
+        marginTop: 15,
+        marginHorizontal: 10,
+        marginBottom: 15,
+        paddingHorizontal: 10
+    },
+    heading1: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 15
+    },
+    text1: {
+        fontWeight: 'bold',
+        fontSize: 15,
+        textAlign: 'center'
+    },
+    text2: {
+        fontWeight: 'bold',
+        fontSize: 14,
+    },
+    topicImage: {
+        width: '100%',
+        height: 80,
+        resizeMode: 'contain',
+        borderRadius: 10,
+        borderColor: 'green',
+        borderWidth: 0.5
+    },
+    singleTopicView: {
+        width: '31%',
+        marginRight: 12
+    },
+    allQuestionsConatiner: {
+        paddingTop: 20,
+        paddingHorizontal: 10
+    },
+    singleQuestionView: {
+        flexDirection: 'row',
+        // borderWidth: 0.4,
+        // borderRadius: 7,
+        // borderColor: '#114B5F',
+        // borderStyle: 'solid',
+        backgroundColor: '#D6DCDC',
+        paddingVertical: 5,
+        marginBottom: 10
+    },
+    profileImageView: {
+        width: '17%',
+        paddingTop: 5,
+        paddingLeft: 5,
+        paddingBottom: 5
+    },
+    profileImage: {
+        width: '90%',
+        height: 50,
+        resizeMode: 'contain',
+        borderRadius: 10,
+        borderColor: 'green',
+        borderWidth: 0.5
+    },
+    questionInfoView: {
+        width: '80%'
+    },
+    dateText: {
+        fontSize: 12,
+        color: 'grey',
+        marginTop: 5
+    },
+    askBtnCont: {
+        width: '90%',
+        marginTop: 20,
+        alignSelf: 'center',
+    },
+    askBtn: {
+        backgroundColor: '#114B5F',
+        height: 45
+    }
+})
 
 export default TopicQuestionsScreen
